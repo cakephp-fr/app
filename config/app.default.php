aa783246385cbb41c2ff4b221df0a69511a37444
cake17 <cake17@cake-websites.com>
2015-10-27 13:20:38 +0100

    CAKEPHP_DEBUG default to true in app.php

--- config/app.default.php
+++ config/app.default.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => env('CAKEPHP_DEBUG', false),
+    'debug' => env('CAKEPHP_DEBUG', true),
 
     /**
      * Configure basic information about the application.
